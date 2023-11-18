095f1be874e656ff0ae827c67ab26adcc4b321d1
DavidLopezIT1 <santyfcw@gmail.com>
2023-11-18 12:37:49 -0500

    First Point Finished

---
+++ Punto-1/index1.php
@@ -0,0 +1,37 @@
+<?php
+
+$select = $_GET["Type_operation"];
+$UserName = $_GET["UserName"];
+$Number1 = $_GET["Number1"];
+$Number2 = $_GET["Number2"];
+
+echo "\n\n\n Interfáz de Resultados\n\n\n";
+
+echo "\n\n\n Buenas tardes $UserName \n\n\n";
+
+echo "Usted eligió como operación " . $select;
+
+
+switch($select){
+    case "Suma":
+      echo "  " . "El resultado de " . $Number1 . " - " . $Number2 . " es =  "  . ($Number1 + $Number2);
+    break;
+    case "Resta":
+            echo "  " . "El resultado de " . $Number1 . " - " . $Number2 . " es =  "  . ($Number1 - $Number2);
+    break;
+    case "Multiplicación":
+      echo "  " . "El resultado de " . $Number1 . " - " . $Number2 . " es =  "  . ($Number1 * $Number2);
+
+    break;
+    case "División":
+      echo "  " . "El resultado de " . $Number1 . " - " . $Number2 . " es =  "  . ($Number1 / $Number2);
+    break;
+    case "Select":
+              echo "   NO ha elegido ningun criterio para su operación";
+    break;
+    
+  }
+
+
+
+?>
---
+++ Punto-2/index2.php
@@ -0,0 +1,7 @@
+<?php
+
+$edad = $_REQUEST[ "age"];
+$nombre = $_REQUEST[ "name"];
+echo "El nombre es : " . $nombre . "\n\nLa edad es : " . $edad ;
+
+?>
