410fc43ab3e65534ea8f4f3038f8e1567463b6db
DavidLopezIT1 <santyfcw@gmail.com>
2023-11-19 22:27:14 -0500

    Brusc-Changes

--- folio-1/index1.php
+++ folio-1/index1.php
@@ -1,20 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style2.css">
+  <title>Resultados</title>
+</head>
+<body>
+
+<?php 
 
 $select = $_GET["Type_operation"];
 $UserName = $_GET["UserName"];
 $Number1 = $_GET["Number1"];
 $Number2 = $_GET["Number2"];
-
-echo "\n\n\n Interfáz de Resultados\n\n\n";
-
-echo "\n\n\n Buenas tardes $UserName \n\n\n";
-
-echo "Usted eligió como operación " . $select;
-
-
+?> 
+<div>
+<span> <?php echo "\n\n\n Interfáz de Resultados\n\n\n"; ?> </span>
+</div>
+<div>
+<span> <?php  echo "\n\n\n Buenas tardes $UserName \n\n\n"; ?> </span>
+</div>
+<div>
+<span> <?php echo "Usted eligió como operación - (" . $select . ")"; ?> </span>
+</div>
+<div>
+<span> <?php 
 switch($select){
     case "Suma":
-      echo "  " . "El resultado de " . $Number1 . " + " . $Number2 . " es =  "  . ($Number1 + $Number2);
+       echo "  " . "El resultado de " . $Number1 . " + " . $Number2 . " es =  "  . ($Number1 + $Number2); 
     break;
     case "Resta":
             echo "  " . "El resultado de " . $Number1 . " - " . $Number2 . " es =  "  . ($Number1 - $Number2);
@@ -34,4 +48,7 @@ switch($select){
 
 
 
-?>
+  ?> </span>
+</div>
+</body>
+</html>
